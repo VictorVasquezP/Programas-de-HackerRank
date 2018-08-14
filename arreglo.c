819ea784f491777df6f29a36918900ca166983fd
VictorVasquezP <42286571+VictorVasquezP@users.noreply.github.com>
2018-08-13 20:20:22 -0500

    Add files via upload

---
+++ arreglo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	{
+		int j, *array, i, suma = 0;
+		scanf("%d", &j);
+		array = (int*) malloc(j*sizeof(int));
+		for(i = 0; i < j; i++) {
+			scanf("%d", array + i);
+		}
+		for(i = 0; i < j; i++) {
+			suma+= *(array+ i);
+		}
+		printf("%d\n", suma);
+		free(array);
+		return 0;
+	}
+	return 0;
+}
+
